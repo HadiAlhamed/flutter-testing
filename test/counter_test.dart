a9faf6130966de3bc55296b3754fba4918659b08
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-01-27 22:26:48 +0300

    group multiple related tests into one group

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,17 +2,21 @@ import 'package:testing/counter.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Counter value should be incremented by one', () {
-    Counter counter = Counter();
-    expect(counter.value, 0);
-    counter.increment();
-    expect(counter.value, 1);
-  });
+  group('Test Counter start, increment and decrement', () {
+    test("Counter should start at 0", () {
+      Counter counter = Counter();
+      expect(counter.value, 0);
+    });
+    test('Counter value should be incremented by one', () {
+      Counter counter = Counter();
+      counter.increment();
+      expect(counter.value, 1);
+    });
 
-  test("Counter value should be decremented by one", () {
-    Counter counter = Counter();
-    expect(counter.value, 0);
-    counter.decrement();
-    expect(counter.value, -1);
+    test("Counter value should be decremented by one", () {
+      Counter counter = Counter();
+      counter.decrement();
+      expect(counter.value, -1);
+    });
   });
 }
